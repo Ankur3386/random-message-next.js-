9ffad1a335da814534a998ab75d3c7bc683ed6b7
Ankur3386 <ankurdeep03386@gmail.com>
2025-02-07 20:19:08 +0530

    schema with typescript and zod

--- src/Schemas/messageSchema.ts
+++ src/Schemas/messageSchema.ts
@@ -1,1 +1,6 @@
- 
+ import {z } from 'zod'
+ export const messageSchema=z.object({
+content:z.string()
+.min(10,{message:'content must me atleast 10 charcter'})
+.max(300,{message:"Content should not exceed more than 300 chharcter"})
+ })
